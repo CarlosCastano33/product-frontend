cae2a0e39a240150ab8c21d79eac9be66e99fc4d
Carlos Castaño <carlose.castano@udea.edu.co>
2025-10-26 17:35:01 -0500

    Frontend para el product-backend

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,16 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('product-frontend');
+  protected title = 'Carlitos';
+
+  setTitle(){
+    this.title = 'El Bueno';
+  }
+  
 }
